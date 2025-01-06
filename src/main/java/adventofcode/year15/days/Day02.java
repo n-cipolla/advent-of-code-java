7fb026fad36aa2a34d6e1e9b670a5e0e9eec052c
Nick Cipolla <ncipolla@trinity.edu>
2025-01-05 23:34:06 -0600

    working on getting 2015 up to date; day 2 complete

--- src/main/java/adventofcode/year15/days/Day02.java
+++ src/main/java/adventofcode/year15/days/Day02.java
@@ -16,19 +16,58 @@ public class Day02 extends Day2015 {
 
     @Override
     public Object part1() {
-        File input = Day01.getResource("day02.txt", 2024);
-        File test = Day01.getResource("test02.txt", 2024);
+        File input = Day01.getResource("day02.txt", 2015);
+        File test = Day01.getResource("test02.txt", 2015);
 
         var lines = Utils.realOrTest(input, test);
-        return 1;
+
+        String[] dims = new String[lines.size()];
+
+        for(int i = 0; i < dims.length; i++) {
+            dims[i] = lines.get(i).trim();
+        }
+
+        int paperTotal = 0;
+
+        for(String s : dims) {
+            String[] dimension = s.split("x");
+            int l = Integer.parseInt(dimension[0]);
+            int w = Integer.parseInt(dimension[1]);
+            int h = Integer.parseInt(dimension[2]);
+
+            int smallest = Math.min(Math.min(l*h, h*w), l*w);
+            paperTotal += smallest + 2*l*h + 2*l*w + 2*h*w;
+        }
+
+        return paperTotal;
     }
 
     @Override
     public Object part2() {
-        File input = Day01.getResource("day02.txt", 2024);
-        File test = Day01.getResource("test02.txt", 2024);
+        File input = Day01.getResource("day02.txt", 2015);
+        File test = Day01.getResource("test02.txt", 2015);
 
         var lines = Utils.realOrTest(input, test);
-        return 1;
+
+        String[] dims = new String[lines.size()];
+
+        for(int i = 0; i < dims.length; i++) {
+            dims[i] = lines.get(i).trim();
+        }
+
+        int ribbonTotal = 0;
+
+        for(String s : dims) {
+            String[] dimension = s.split("x");
+            int l = Integer.parseInt(dimension[0]);
+            int w = Integer.parseInt(dimension[1]);
+            int h = Integer.parseInt(dimension[2]);
+
+            int perim = 2 * Math.min(Math.min(l+w,w+h), h+l);
+            int vol = l*w*h;
+            ribbonTotal += perim + vol;
+        }
+
+        return ribbonTotal;
     }
 }
